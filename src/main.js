f5b7428be9caf050aab9f9418e727ac59937116d
max <93793772+maxhithere@users.noreply.github.com>
2022-11-27 22:46:15 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 const session = require("express-session");
 const passport = require("./strategies/config");
 const db = require("./db/db_init");
+let indexFile;
 
 db.then(() => console.log("Connected to database")).catch((err) =>
   console.log(err)
@@ -25,33 +26,37 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get("/", (req, res) => {
+    res.send('s'); //replace here
+})
+
 app.get(
-  "/",
+  "/auth",
   passport.authenticate("discord", {
     scope: ["identify", "email", "guilds"],
-    prompt: "idk",
+    prompt: "authorize",
   }),
   function (req, res) {}
 );
 app.get(
-  "/callback",
+  "/auth/redirect",
   passport.authenticate("discord", { failureRedirect: "/" }),
   function (req, res) {
-    res.redirect("/info");
+    res.redirect("/dashboard");
   } // auth success
 );
 app.get("/logout", function (req, res) {
   req.logout();
   res.redirect("/");
 });
-app.get("/info", checkAuth, function (req, res) {
-  //console.log(req.user)
-  res.json(req.user);
+app.get("/dashboard", checkAuth, function (req, res) {
+ 
+  res.json(req.user.username);
 });
 
 function checkAuth(req, res, next) {
   if (req.isAuthenticated()) return next();
-  res.send("not logged in :(");
+  res.redirect("/auth");
 }
 
 app.listen(port, () => {
