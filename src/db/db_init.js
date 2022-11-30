36e5a8ca8c37e8740271ed68ff0fdab9926b551c
max <93793772+maxhithere@users.noreply.github.com>
2022-11-30 15:40:48 -0500

    Update db_init.js

--- src/db/db_init.js
+++ src/db/db_init.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//replace "mongodb://localhost:27017" if you are not in a local hosting environment
